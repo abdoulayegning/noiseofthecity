d87da9c8419f1d1c6633955387b1bb6f726e1a03
Abdoulaye GNING <abdoulaye@lapetitetribu.co>
2022-05-23 13:34:54 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template> 
+  <div>
+    <router-view></router-view>  
+  </div>
+</template>
+
+<script> 
+ 
+export default {
+  name: 'App',
+  data(){
+      return {
+           
+      }
+  }, 
+  mounted(){
+     
+  }
+}
+</script>
+
+<style> 
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  background-image: url('./assets/static.jpeg');  
+}
+
+#sketch { 
+    position: absolute;
+    z-index: -1000;
+    top: 0;
+    left: 0; 
+}
+ 
+
+@font-face {
+    font-family: 'Stopwatch';
+    src: url('./assets/stopwatch/StopwatchRegular.eot');
+    src: url('./assets/stopwatch/StopwatchRegular.eot?#iefix') format('embedded-opentype'),
+        url('./assets/stopwatch/StopwatchRegular.woff2') format('woff2'),
+        url('./assets/stopwatch/StopwatchRegular.woff') format('woff'),
+        url('./assets/stopwatch/StopwatchRegular.ttf') format('truetype'),
+        url('./assets/stopwatch/StopwatchRegular.svg#StopwatchRegular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+
+</style>
---
+++ src/views/Track.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="wrapper"> 
+    <div id="logo"><img src="../assets/logo.png" alt=""></div>
+    <div id="info">
+      <div id="cover"><img src="../assets/1-.png" alt=""></div>
+      <div id="order">{{msg}}</div>
+      <div id="title">Lorem ipsum dolor</div>
+      <div id="description">
+        Fa sa mbëgeel nek, sa cofeel fees fa dell, foofee, moy sa dëkkuway.<br> 
+                Là où il y a de l'amour, là où il y a du plaisir, c'est là qu'est la patrie.<br> 
+                Where love is, where there is delight, that is where the homeland is.<br>
+      </div>
+    </div>
+    <div id="player">
+      <button>Play</button><button>Stop</button>
+    </div>
+  </div>
+</template>
+
+<style scoped> 
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap');
+ 
+#logo { 
+  width: 100%;
+  margin-top: 5vh;
+} 
+
+#logo img {
+  width: 35%;
+  display: block;
+  margin: 0 auto;
+}
+
+#player button {
+  color: white;
+  padding-left: 1.25em;
+  padding-right: 1.25em;
+  padding-top: .45em;
+  padding-bottom: .45em;
+  font-family: Inconsolata;
+  background-color: black;
+  border: 1px solid white;
+}
+
+#description {
+  font-family: Inconsolata;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em; 
+  width: 85%; 
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 14px; 
+}
+
+#info {
+  text-align: center;
+  color: white;
+  font-family: Stopwatch;
+}
+
+
+#title {
+  font-size: 35px;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  text-transform: uppercase;
+  width: 85%; 
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#player {
+  position: absolute;
+  bottom: 5%;
+  left: 0;
+  display: flex;
+  width: 100%; 
+  justify-content: center;
+}
+
+#player button {
+  margin-left: 1em;
+  margin-right: 1em;
+}
+
+#cover img {
+  width: 50%;
+  margin-top: 1.5em; 
+}
+
+#order  { 
+  margin-top: 1.5em; 
+}
+</style>
+
+<script>
+export default {
+  name: 'Track',
+  data(){
+      return {
+          msg: this.$route.params.track
+      }
+  } 
+}
+</script>
